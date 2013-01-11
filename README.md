5413d3d617a1d9d5126c907bebc1be59fa26b974
Andreas Watterott <aw@watterott.net>
2013-01-11 21:50:38 +0100

    added layout and source files

---
+++ README.md
@@ -0,0 +1,14 @@
+# TSL45315-Breakout
+Breakout with TSL45315 digital ambient light sensors
+
+<http://www.watterott.com/en/TSL45315-Breakout>
+
+
+## Features
+ * 3V3 - 5V tolerant I2C/TWI interface
+
+
+## Directories
+ /pcb - Schematics and Layout files
+ 
+ /src - Software Examples
---
+++ src/TSL45315.ino
@@ -0,0 +1,74 @@
+#include <Wire.h>
+
+/*
+  Example sketch for TSL45315-Breakout.
+
+  Board	          I2C/TWI Pins
+                  SDA, SCL
+  ----------------------------
+  Uno, Ethernet   A4, A5
+  Mega            20, 21
+  Leonardo         2,  3
+  Due             20, 21
+*/
+
+#define I2C_ADDR     (0x29)
+
+#define REG_CONTROL  0x00
+#define REG_CONFIG   0x01
+#define REG_DATALOW  0x04
+#define REG_DATAHIGH 0x05
+#define REG_ID       0x0A
+
+void setup()
+{
+   Serial.begin(9600);
+   Serial.println("Wait for input");
+   while(Serial.available() == 0);
+   while(Serial.available() != 0){ Serial.read(); }
+
+   Wire.begin();
+
+   Serial.print("ID: ");
+   Wire.beginTransmission(I2C_ADDR);
+   Wire.write(0x80|REG_ID);
+   Wire.endTransmission();
+   Wire.requestFrom(I2C_ADDR, 1); //request 1 byte
+   while(Wire.available())
+   {
+     unsigned char c = Wire.read();
+     Serial.print(c&0xF0, HEX);
+   }
+   Serial.println("");
+
+   Serial.println("Power on...");
+   Wire.beginTransmission(I2C_ADDR);
+   Wire.write(0x80|REG_CONTROL);
+   Wire.write(0x03); //power on
+   Wire.endTransmission();
+
+   Serial.println("Config...");
+   Wire.beginTransmission(I2C_ADDR);
+   Wire.write(0x80|REG_CONFIG);
+   Wire.write(0x00); //M=1 T=400ms
+   Wire.endTransmission();
+}
+
+void loop()
+{
+   unsigned char a, b;
+   unsigned int lux;
+
+   Wire.beginTransmission(I2C_ADDR);
+   Wire.write(0x80|REG_DATALOW);
+   Wire.endTransmission();
+   Wire.requestFrom(I2C_ADDR, 2); //request 2 bytes
+   a = Wire.read();
+   b = Wire.read();
+   while(Wire.available()){ Wire.read(); } //received more bytes?
+   lux = 1 * ( (b<<8) | (a<<0) ); //multiplier 1
+   Serial.print("Lux: ");
+   Serial.println(lux, DEC);
+
+   delay(1000);
+}
